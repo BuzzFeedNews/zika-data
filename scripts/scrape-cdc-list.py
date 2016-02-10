36f82ed42efebf9c533304b032045bb72fb099af
Jeremy Singer-Vine <jsvine@gmail.com>
2016-02-10 18:00:03 -0500

    Tweak CDC country script

--- scripts/scrape-cdc-list.py
+++ scripts/scrape-cdc-list.py
@@ -3,7 +3,7 @@ import requests
 import lxml.html
 
 def scrape():
-    res = requests.get("http://www.cdc.gov/zika/geo/index.html")
+    res = requests.get("http://www.cdc.gov/zika/geo/active-countries.html")
     dom = lxml.html.fromstring(res.content)
     li_els = dom.cssselect(".list-block li")
     countries = [ li.text_content() for li in li_els ]
