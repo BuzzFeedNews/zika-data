1b95969110f97af397cb3314b59c30679911da48
Jeremy Singer-Vine <jsvine@gmail.com>
2016-03-18 16:10:57 -0400

    Update CDC scraper to handle new format

--- scripts/scrape-cdc-state-case-counts.py
+++ scripts/scrape-cdc-state-case-counts.py
@@ -2,12 +2,18 @@
 import requests
 import lxml.html
 import pandas as pd
+import re
 import sys
 
 URL = "http://www.cdc.gov/zika/geo/united-states.html"
 
 INT_COLS = [ "travel_associated_cases", "locally_acquired_cases" ]
 COLS = [ "state_or_territory" ] + INT_COLS
+
+paren_pat = re.compile(r"\([^\)]+\)") 
+
+def parse_cell(text):
+    return re.sub(paren_pat, "", text).strip()
 
 def scrape():
     html = requests.get(URL).content
@@ -16,7 +22,7 @@ def scrape():
     table = dom.cssselect("table")[0]
     rows = table.cssselect("tr")
 
-    cells = [ [ td.text_content().strip() 
+    cells = [ [ parse_cell(td.text_content())
         for td in tr.cssselect("td") ] 
              for tr in rows ]
 
